890ba6445ee94147054483f2d7b48052f01cb5a0
Alex2manov <144853661+Alex2manov@users.noreply.github.com>
2024-01-12 01:47:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -159,3 +159,69 @@ $(".FAQ h5:first").addClass("active");
    $(this).toggleClass("active");
    $(this).siblings("h5").removeClass("active");
  });
+
+
+
+
+$(document).ready(function() {
+  $('.feedback-slider').slick({
+    infinite: true,
+    speed: 500,
+    prevArrow: $("#feedback-previous"),
+    nextArrow: $("#feedback-next"),
+    fade: true,
+    swipe: false, 
+    draggable: false,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+  
+  $('.feedback-slider').on('afterChange', function(event, slick, currentSlide) {
+    $('#feedback-number').text('0' + (currentSlide + 1));
+  });
+
+  $("#wwu-slider-1").slick({
+    infinite: true,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '10%',
+    autoplaySpeed: 3500,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {slidesToShow: 3}
+        },
+        {
+            breakpoint: 600, 
+            settings: {slidesToShow: 2}
+        }
+    ]
+  });
+
+  $("#wwu-slider-2").slick({
+    infinite: true,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '10%',
+    autoplaySpeed: 3500,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {slidesToShow: 3}
+        },
+        {
+            breakpoint:600, 
+            settings: {slidesToShow: 2}
+        }
+    ]
+  });
+});
+
